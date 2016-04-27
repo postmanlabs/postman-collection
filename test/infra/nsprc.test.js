ff694d74c9cebbea2e577ed1ae2a9bfd095dcb06
Shamasis Bhattacharya <mail@shamasis.net>
2016-04-27 12:46:14 +0530

    Added fake nsp scripts to exclude "marked" vulnerability. Also added infra tests for the same

---
+++ test/infra/nsprc.test.js
@@ -0,0 +1,27 @@
+/**
+ * @fileOverview Ensures nsprc is as expected
+ */
+
+var expect = require('expect.js'),
+    fs = require('fs');
+
+/* global describe, it, before */
+describe('nsprc', function () {
+    var nsprc;
+
+    before(function () {
+        nsprc = JSON.parse(fs.readFileSync('./.nsprc').toString());
+    });
+
+    it('must exist', function () {
+        expect(nsprc).to.be.ok();
+    });
+
+    it('must not have any exclusion', function () {
+        expect(nsprc.exceptions).to.eql([]);
+    });
+
+    it('must exclude only a known set of packages', function () {
+        expect(nsprc.exclusions).to.eql(['marked']);
+    });
+});
