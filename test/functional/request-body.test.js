346e5a0689e92c3101be07064bba827e56eaf2ff
Aniket Panse <aniket@getpostman.com>
2016-06-06 13:20:00 +0530

    added a new mode for request body

--- test/functional/request-body.test.js
+++ test/functional/request-body.test.js
@@ -19,4 +19,41 @@ describe('RequestBody', function () {
         reqData.mode = 'urlencoded';
         expect(reqData.toString()).to.eql(expectedQueryData);
     });
+
+    it('should support file mode', function () {
+        var rawBody = {
+                mode: 'formdata',
+                formdata: [
+                    {
+                        key: 'hiya',
+                        value: 'heyo'
+                    },
+                    {
+                        key: 'alpha',
+                        value: 'beta'
+                    }
+                ],
+                raw: 'abhijitkane',
+                urlencoded: [
+                    {
+                        key: 'haha',
+                        value: 'somevalue'
+                    }
+                ],
+                file: {
+                    ref: '/somewhere/in/the/file/system'
+                }
+            },
+            reqData = new RequestBody(rawBody);
+        expect(reqData.file).to.have.property('ref', rawBody.file.ref);
+    });
+
+    it('should convert file reference to object, if only the reference is given', function () {
+        var rawBody = {
+                mode: 'file',
+                file: '/omg/where/is/this/file'
+            },
+            reqData = new RequestBody(rawBody);
+        expect(reqData.file).to.have.property('ref', rawBody.file);
+    });
 });
