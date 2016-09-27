55bc2c1127ef6c669c1a26acaaeaa9f54139d44b
Shamasis Bhattacharya <mail@shamasis.net>
2016-09-27 17:15:03 +0530

    Streamlined unit-test functions

--- npm/test-unit.js
+++ npm/test-unit.js
@@ -8,32 +8,43 @@ require('colors');
 // set directories and files for test and coverage report
 var path = require('path'),
 
-    IS_WINDOWS = (/^win/).test(process.platform);
+    IS_WINDOWS = (/^win/).test(process.platform),
+    COV_REPORT_PATH = '.coverage',
+    REPORT_PATH = path.join('.tmp', 'report.xml'),
+    SPEC_SOURCES = path.join('test', 'unit');
 
 module.exports = function (exit) {
-    var mochaReporter,
-        istanbulReport;
+    var specPattern = (process.argv[2] || '.*'),
+        mochaReporter = 'spec',
+        istanbulReport = '';
 
+    // for CI, we use simple xunit reporter
     if (process.env.CI) {
         mochaReporter = 'xunit';
         istanbulReport = '--report cobertura';
-    }
-    else {
-        mochaReporter = 'spec';
-        istanbulReport = '';
     }
 
     // banner line
     console.log('Running unit tests using mocha on node...'.yellow.bold);
 
     mkdir('-p', '.tmp');
-    test('-d', '.coverage') && rm('-rf', '.coverage') && mkdir('-p', '.coverage');
+    test('-d', COV_REPORT_PATH) && rm('-rf', COV_REPORT_PATH) && mkdir('-p', COV_REPORT_PATH);
 
-    // eslint-disable-next-line max-len
-    exec((IS_WINDOWS ? '' : 'node ') + '"' + path.join('node_modules', '.bin', 'istanbul') + (IS_WINDOWS ? '.cmd' : '') +
-        '" cover ' + istanbulReport + ' --dir .coverage --color --print both ' + path.join('node_modules', 'mocha', 'bin', '_mocha') +
-        ' -- --reporter ' + mochaReporter + ' --reporter-options output=' + path.join('.tmp', 'report.xml') + path.join('test', 'unit') +
-        ' --recursive --prof --colors --grep=' + (process.argv[2] || '.*'), exit);
+    // windows istanbul and mocha commands need some special attention.
+    if (IS_WINDOWS) {
+        // sample command in case you're confused
+        // node_modules\.bin\istanbul.cmd cover  --dir .coverage --color --print both
+        //      node_modules\mocha\bin\_mocha -- --reporter spec --reporter-options output=
+        //      .tmp\report.xml test\unit --recursive --prof --colors --grep=.*
+        exec(`node_modules\\.bin\\istanbul.cmd cover ${istanbulReport} --dir ${COV_REPORT_PATH} --colors ` +
+            `--print both node_modules\\mocha\\bin\\_mocha -- ${SPEC_SOURCES} --reporter ${mochaReporter} ` +
+            `--reporter-options output=${REPORT_PATH} --recursive --prof --colors --grep=${specPattern}`, exit);
+    }
+    else {
+        exec(`./node_modules/.bin/istanbul cover ${istanbulReport} --dir ${COV_REPORT_PATH} --colors ` +
+            `--print both node_modules/mocha/bin/_mocha -- ${SPEC_SOURCES} --reporter ${mochaReporter} ` +
+            `--reporter-options output=${REPORT_PATH} --recursive --prof --colors --grep=${specPattern}`, exit);
+    }
 };
 
 // ensure we run this script exports if this is a direct stdin.tty run
