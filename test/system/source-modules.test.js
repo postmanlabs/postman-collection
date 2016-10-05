e7c6e0b3d13759aad990e9ce1cc2c05ca3760398
Aniket Panse <aniket@getpostman.com>
2016-10-05 16:41:36 +0530

    fixed tests

--- test/system/source-modules.test.js
+++ test/system/source-modules.test.js
@@ -10,7 +10,7 @@ var fs = require('fs'),
 
     BASELESS_MODULES = ['Description'],
     SCHEMALESS_MODULES = ['EventList', 'FormParam', 'PropertyBase', 'PropertyList', 'Property', 'QueryParam',
-        'RequestAuth', 'RequestBody', 'VariableList', 'VariableScope'];
+        'RequestAuth', 'RequestBody', 'VariableList', 'VariableScope', 'CookieJar', 'CookieList'];
 
 describe('collection module', function () {
     var modules = require('require-all')({
