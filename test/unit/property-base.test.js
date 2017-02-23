58c8ada872668489cd8c4e76ed579fd238a255f8
kunagpal <kunal@getpostman.com>
2017-02-23 12:35:36 +0530

    Cleaned up tardiness

--- test/unit/property-base.test.js
+++ test/unit/property-base.test.js
@@ -1,6 +1,6 @@
 var _ = require('lodash'),
     expect = require('expect.js'),
-    Sdk = require('../../lib/index.js'),
+    sdk = require('../../lib/index.js'),
 
     fixtures = require('../fixtures');
 
@@ -13,7 +13,7 @@ describe('PropertyBase', function () {
                     _postman_two: 'something',
                     _three: _.noop
                 },
-                base = new Sdk.PropertyBase(definition);
+                base = new sdk.PropertyBase(definition);
 
             expect(base.meta().postman_one).to.eql(definition._postman_one);
             expect(base.meta()).to.have.property('postman_two', definition._postman_two);
@@ -26,7 +26,7 @@ describe('PropertyBase', function () {
                     _postman_two: 'something',
                     _three: _.noop
                 },
-                base = new Sdk.PropertyBase(definition);
+                base = new sdk.PropertyBase(definition);
 
             expect(base.meta('postman_one').postman_one).to.eql(definition._postman_one);
             expect(base.meta('postman_one')).to.not.have.property('postman_two');
@@ -35,40 +35,24 @@ describe('PropertyBase', function () {
     });
 
     describe('forEachParent helper', function () {
-        var expectedOrder = ['F2', 'F2.F3'],
-            collection = new Sdk.Collection(fixtures.nestedCollectionV2),
+        var collection = new sdk.Collection(fixtures.nestedCollectionV2),
             item = collection.items.members[1].items.members[0].items.members[0];
 
         it('should set default options correctly when none are specified', function () {
-            var chain = [];
+            var chain = [],
+                expectedOrder = ['F2', 'F2.F3'];
 
             item.forEachParent(chain.unshift.bind(chain));
             expect(_.map(chain, 'name')).to.eql(expectedOrder);
         });
 
         it('should return the collection as well when required', function () {
-            var chain = [];
+            var chain = [],
+                expectedOrder = ['F2', 'F2.F3'];
 
             item.forEachParent({ withRoot: true }, chain.unshift.bind(chain));
             expect(chain.shift().id).to.be(fixtures.nestedCollectionV2.info._postman_id);
             expect(_.map(chain, 'name')).to.eql(expectedOrder);
-        });
-
-        describe('singleton options', function () {
-            it('should be handled correctly with withRoot set to false', function () {
-                var chain = [];
-
-                item.forEachParent(false, chain.unshift.bind(chain));
-                expect(_.map(chain, 'name')).to.eql(expectedOrder);
-            });
-
-            it('should be handled correctly with withRoot set to true', function () {
-                var chain = [];
-
-                item.forEachParent(true, chain.unshift.bind(chain));
-                expect(chain.shift().id).to.be(fixtures.nestedCollectionV2.info._postman_id);
-                expect(_.map(chain, 'name')).to.eql(expectedOrder);
-            });
         });
     });
 });
