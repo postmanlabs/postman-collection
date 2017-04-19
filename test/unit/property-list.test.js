22e5fefb0802b9b65d57e6548d92375906c3aa2e
Shamasis Bhattacharya <mail@shamasis.net>
2017-04-19 14:15:59 +0530

    Added .toObject functionality in property-list

--- test/unit/property-list.test.js
+++ test/unit/property-list.test.js
@@ -499,4 +499,55 @@ describe('PropertyList', function () {
             expect(PropertyList.isPropertyList()).to.be(false);
         });
     });
+
+    describe('toObject', function () {
+        var FakeType = function (options) {
+                this.keyAttr = options.keyAttr;
+                this.value = options.value;
+            };
+
+        FakeType._postman_propertyIndexKey = 'keyAttr';
+        FakeType._postman_propertyIndexCaseInsensitive = true;
+        FakeType._postman_propertyAllowsMultipleValues = true;
+        FakeType.prototype.valueOf = function () {
+            return this.value;
+        };
+
+        it('should return a pojo', function () {
+            var list = new PropertyList(FakeType, {}, [{
+                keyAttr: 'key1',
+                value: 'val1'
+            }, {
+                keyAttr: 'key1',
+                value: 'val2'
+            }, {
+                keyAttr: 'key2',
+                value: 'val3'
+            }]);
+
+            expect(list.toObject()).to.eql({
+                key1: 'val2',
+                key2: 'val3'
+            });
+        });
+
+        it('should be able to override case sensitivity return a pojo', function () {
+            var list = new PropertyList(FakeType, {}, [{
+                keyAttr: 'key1',
+                value: 'val1'
+            }, {
+                keyAttr: 'KEY1',
+                value: 'val2'
+            }, {
+                keyAttr: 'key2',
+                value: 'val3'
+            }]);
+
+            expect(list.toObject(true)).to.eql({
+                key1: 'val1',
+                KEY1: 'val2',
+                key2: 'val3'
+            });
+        });
+    });
 });
