4806837d374e3cdde67aa98f47b5eb5b7b393214
Aniket Panse <aniketpanse@gmail.com>
2017-05-11 17:20:52 +0530

    added a .contains() to header list

--- test/unit/header-list.test.js
+++ test/unit/header-list.test.js
@@ -23,4 +23,12 @@ describe('HeaderList', function () {
         var hl = new HeaderList(null, 'Accept: *\nContent-Type: text/html');
         expect(hl.contentSize(200, 'OK')).eql(38);
     });
+
+    describe('.contains()', function () {
+        it('should return true if the header list contains given headers', function () {
+            var hl = new HeaderList(null, 'Accept: *\nContent-Type: text/html');
+
+
+        });
+    });
 });
