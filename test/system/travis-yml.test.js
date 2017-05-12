ee7234103158745567adec4c134b747a4feeea62
Kunal Nagpal <kunagpal@gmail.com>
2017-05-12 19:03:10 +0530

    Added system tests for updated Travis build configuration

--- test/system/travis-yml.test.js
+++ test/system/travis-yml.test.js
@@ -1,4 +1,5 @@
 /* global describe, it */
+/* eslint-env node, es6 */
 var expect = require('expect.js');
 
 describe('travis.yml', function () {
@@ -23,9 +24,24 @@ describe('travis.yml', function () {
     });
 
     describe('structure', function () {
+        it('should use the trusty Ubuntu distribution', function () {
+            expect(travisYAML.dist).to.be('trusty');
+        });
+
         it('language must be set to node', function () {
             expect(travisYAML.language).to.be('node_js');
             expect(travisYAML.node_js).to.eql(['4', '6']);
+        });
+
+        it('should have a valid before_install sequence', function () {
+            expect(travisYAML.before_install).to.have.property(0, 'export CHROME_BIN=google-chrome');
+            expect(travisYAML.before_install).to.have.property(1, 'export DISPLAY=:99.0');
+            expect(travisYAML.before_install).to.have.property(2, 'sh -e /etc/init.d/xvfb start');
+            expect(travisYAML.before_install).to.have.property(3, 'sleep 3');
+        });
+
+        it('should use the stable google chrome package', function () {
+            expect(travisYAML.addons).to.eql({ apt: { packages: ['google-chrome-stable'] } });
         });
     });
 });
