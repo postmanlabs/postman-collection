fdf28da2dd60f0979dff22bd8b71c13f4199b9b6
Kunal Nagpal <kunagpal@gmail.com>
2017-06-27 18:57:29 +0530

    Unified Cookie tests

--- test/unit/cookie.test.js
+++ test/unit/cookie.test.js
@@ -4,6 +4,63 @@ var expect = require('expect.js'),
 
 /* global describe, it */
 describe('Cookie', function () {
+    describe('sanity', function () {
+        var rawCookie = fixtures.collectionV2.item[0].response[0].cookie[0],
+            cookie = new Cookie(rawCookie);
+
+        it('initializes successfully', function () {
+            expect(cookie).to.be.ok();
+        });
+
+        describe('has property', function () {
+            it('domain', function () {
+                expect(cookie).to.have.property('domain', rawCookie.domain);
+            });
+
+            it('expires', function () {
+                expect(cookie).to.have.property('expires', rawCookie.expires);
+            });
+
+            it('hostOnly', function () {
+                expect(cookie).to.have.property('hostOnly', rawCookie.hostOnly);
+            });
+
+            it('httpOnly', function () {
+                expect(cookie).to.have.property('httpOnly', rawCookie.httpOnly);
+            });
+
+            it.skip('maxAge', function () { // @todo: possibly delete test. seems like based on old expectations
+                expect(cookie).to.have.property('maxAge', undefined);
+            });
+
+            it('path', function () {
+                expect(cookie).to.have.property('path', rawCookie.path);
+            });
+
+            it('secure', function () {
+                expect(cookie).to.have.property('secure', rawCookie.secure);
+            });
+
+            it('session', function () {
+                expect(cookie).to.have.property('session', rawCookie.session);
+            });
+
+            it('value', function () {
+                expect(cookie).to.have.property('value', rawCookie.value);
+            });
+
+            it('_', function () {
+                expect(cookie).to.have.property('_');
+                expect(cookie._).to.have.property('postman_storeId', rawCookie._postman_storeId);
+            });
+
+            it('update', function () {
+                expect(cookie.update).to.be.ok();
+                expect(cookie.update).to.be.an('function');
+            });
+        });
+    });
+
     describe('json representation', function () {
         it('must match what it was initialized with', function () {
             var rawCookie = fixtures.collectionV2.item[0].response[0].cookie[0],
