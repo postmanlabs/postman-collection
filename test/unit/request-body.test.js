fe532ff65db6585d88248df9d42fe870837c99d7
Kunal Nagpal <kunagpal@gmail.com>
2017-06-27 19:07:07 +0530

    Unified RequestBody tests

--- test/unit/request-body.test.js
+++ test/unit/request-body.test.js
@@ -1,7 +1,8 @@
 var expect = require('expect.js'),
     rawRequestBody = require('../fixtures').requestData,
     RequestBody = require('../../lib/index.js').RequestBody,
-    QueryParam = require('../../lib/index.js').QueryParam;
+    QueryParam = require('../../lib/index.js').QueryParam,
+    PropertyList = require('../../lib/index.js').PropertyList;
 
 /* global describe, it */
 describe('RequestBody', function () {
@@ -75,6 +76,25 @@ describe('RequestBody', function () {
         expect(reqData.formdata.toObject()).to.eql({
             hiya: 'heyo',
             alpha: 'beta'
+        });
+    });
+
+    describe('sanity', function () {
+        it('should be parsed properly', function () {
+            var reqData = new RequestBody(rawRequestBody);
+            expect(reqData).to.be.ok();
+            expect(reqData).to.have.property('mode', rawRequestBody.mode);
+
+            // Raw Request body
+            expect(reqData).to.have.property('raw', rawRequestBody.raw);
+
+            // Form data
+            expect(reqData.formdata).to.be.a(PropertyList);
+            expect(reqData.formdata.count()).to.eql(rawRequestBody.formdata.length);
+
+            // URL Encoded parameters
+            expect(reqData.urlencoded).to.be.a(PropertyList);
+            expect(reqData.urlencoded.count()).to.eql(rawRequestBody.urlencoded.length);
         });
     });
 
