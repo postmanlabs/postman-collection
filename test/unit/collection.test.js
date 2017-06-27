d684f9e1b4eb150684c0c7e7e9ab52374b7928a4
Kunal Nagpal <kunagpal@gmail.com>
2017-06-27 18:55:42 +0530

    Unified Collection tests

--- test/unit/collection.test.js
+++ test/unit/collection.test.js
@@ -1,8 +1,72 @@
 var expect = require('expect.js'),
-    Collection = require('../../lib/index.js').Collection;
+    Collection = require('../../lib/index.js').Collection,
+
+    fixtures = require('../fixtures');
 
 /* global describe, it */
 describe('Collection', function () {
+    describe('sanity', function () {
+        var rawCollection = fixtures.collectionV2,
+            collection = new Collection(rawCollection);
+
+        it('initializes successfully', function () {
+            expect(collection).to.be.ok();
+        });
+
+        describe('has property', function () {
+            it('id', function () {
+                expect(collection).to.have.property('id', rawCollection.info.id);
+            });
+
+            it('items', function () {
+                expect(collection).to.have.property('items');
+                expect(collection.items).to.be.an('object');
+                expect(collection.items.all()).to.be.an('array');
+                expect(collection.items.all()).to.not.be.empty();
+            });
+
+            it('name', function () {
+                expect(collection).to.have.property('name', rawCollection.info.name);
+            });
+
+            it('events', function () {
+                expect(collection).to.have.property('events');
+                expect(collection.events.all()).to.be.an('array');
+                expect(collection.events.all()).to.not.be.empty();
+            });
+        });
+
+        describe('has function', function () {
+            it('forEachItem', function () {
+                expect(collection.forEachItem).to.be.a('function');
+            });
+        });
+
+        describe('info block parsing', function () {
+            it('must parse description', function () {
+                var collection = new Collection({
+                    info: {
+                        name: 'test',
+                        description: 'this is test description'
+                    }
+                });
+
+                expect(collection.description).be.ok();
+                expect(collection.description.toString()).be('this is test description');
+            });
+
+            it('must parse description from outside info block if info is absent', function () {
+                var collection = new Collection({
+                    name: 'test',
+                    description: 'this is test description'
+                });
+
+                expect(collection.description).be.ok();
+                expect(collection.description.toString()).be('this is test description');
+            });
+        });
+    });
+
     describe('isCollection', function () {
         it('must distinguish between collections and other objects', function () {
             var collection = new Collection(),
