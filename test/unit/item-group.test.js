08022b90c0eb7b16a144cb66f4a66902448674a8
Kunal Nagpal <kunagpal@gmail.com>
2017-06-27 19:03:07 +0530

    Unified ItemGroup tests

--- test/unit/item-group.test.js
+++ test/unit/item-group.test.js
@@ -64,6 +64,44 @@ describe('ItemGroup', function () {
         expect(_.omit(group.toJSON(), 'id')).to.eql({ name: 'Blank folder', item: [], event: [] });
     });
 
+    describe('sanity', function () {
+        var rawItemGroup = fixtures.collectionV2,
+            itemGroup = new ItemGroup(rawItemGroup);
+
+        it('initializes successfully', function () {
+            expect(itemGroup).to.be.ok();
+        });
+
+        describe('has property', function () {
+            it('id', function () {
+                expect(itemGroup).to.have.property('id', rawItemGroup.info.id);
+            });
+
+            it('item', function () {
+                expect(itemGroup).to.have.property('items');
+                expect(itemGroup.items).to.be.an('object');
+                expect(itemGroup.items.all()).to.be.an('array');
+                expect(itemGroup.items.all()).to.not.be.empty();
+            });
+
+            it('name', function () {
+                expect(itemGroup).to.have.property('name', rawItemGroup.info.name);
+            });
+            it('events', function () {
+                expect(itemGroup).to.have.property('events');
+                expect(itemGroup.events.all()).to.be.an('array');
+                expect(itemGroup.events.all()).to.not.be.empty();
+            });
+        });
+
+        describe('has function', function () {
+            it('forEachItem', function () {
+                expect(itemGroup.forEachItem).to.be.ok();
+                expect(itemGroup.forEachItem).to.be.a('function');
+            });
+        });
+    });
+
     describe('.parent checks', function () {
         var collection = new Collection(fixtures.nestedCollectionV2);
 
