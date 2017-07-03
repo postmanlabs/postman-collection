4095e7cc323a868fdd78f0626056947d31b5ff48
Harendra Singh <66hari@gmail.com>
2017-07-03 17:35:08 +0530

    Fix the bug where proxyConfig.match is not updating from another instance of proxyConfig

--- test/integration/proxy-config.test.js
+++ test/integration/proxy-config.test.js
@@ -1,6 +1,8 @@
 var expect = require('expect.js'),
     ProxyConfig = require('../../lib/index.js').ProxyConfig,
-    DEFAULT_PATTERN = 'http+https://*/*';
+    DEFAULT_PATTERN = 'http+https://*/*',
+    DEFAULT_HOST = '',
+    DEFAULT_PORT = 8080;
 
 /* global describe, it */
 describe('Proxy Config', function () {
@@ -9,6 +11,8 @@ describe('Proxy Config', function () {
 
         expect(p.match.pattern).to.be(DEFAULT_PATTERN);
         expect(p.tunnel).to.be(false);
+        expect(p.host).to.be(DEFAULT_HOST);
+        expect(p.port).to.be(DEFAULT_PORT);
     });
 
     it('should prepopulate the values when pass through the constructor', function () {
@@ -42,5 +46,35 @@ describe('Proxy Config', function () {
         expect(p.host).to.be('proxy.com');
         expect(p.tunnel).to.be(false);
         expect(p.disabled).to.not.be.ok();
+    });
+
+    it('should update the properties', function () {
+        var p1 = new ProxyConfig(),
+            newMatch = 'https://google.com/*',
+            newHost = 'new-host',
+            newPort = 9090,
+            p2 = new ProxyConfig({
+                match: newMatch,
+                host: newHost,
+                port: newPort,
+                tunnel: true
+            });
+
+        expect(p1.match.pattern).to.be(DEFAULT_PATTERN);
+        expect(p1.host).to.be(DEFAULT_HOST);
+        expect(p1.port).to.be(DEFAULT_PORT);
+        expect(p1.tunnel).to.be(false);
+
+        expect(p2.match.pattern).to.be(newMatch);
+        expect(p2.host).to.be(newHost);
+        expect(p2.port).to.be(newPort);
+        expect(p2.tunnel).to.be(true);
+
+        p1.update(p2);
+
+        expect(p1.match.pattern).to.be(newMatch);
+        expect(p1.host).to.be(newHost);
+        expect(p1.port).to.be(newPort);
+        expect(p1.tunnel).to.be(true);
     });
 });
