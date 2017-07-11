5ebbb7b8ee41ff903d93324b1e19b3f791ba70d8
Kunal Nagpal <kunagpal@gmail.com>
2017-07-12 01:00:10 +0530

    Removed schema related system tests

--- test/system/source-modules.test.js
+++ test/system/source-modules.test.js
@@ -1,27 +1,19 @@
 /* global describe, it */
-var fs = require('fs'),
-    path = require('path'),
+var path = require('path'),
 
     _ = require('lodash'),
     expect = require('expect.js'),
 
     sdk = require('../../lib'),
 
-    BASELESS_MODULES = ['Description'],
-    SCHEMALESS_MODULES = ['EventList', 'FormParam', 'PropertyBase', 'PropertyList', 'Property', 'ProxyConfigList',
-        'QueryParam', 'RequestAuth', 'RequestBody', 'VariableList', 'VariableScope', 'RequestAuthBase'];
+    BASELESS_MODULES = ['Description'];
 
 describe('collection module', function () {
     var modules = require('require-all')({
-            dirname: path.join(__dirname, '/../../lib/collection'),
-            excludeDirs: /^\.(git|svn)$/,
-            recursive: true
-        }),
-        schemas = fs.readdirSync(path.join(__dirname, '/../../lib/schema')).filter(function (file) {
-            return (/^.*\.json$/g).test(file);
-        }).map(function (file) {
-            return file.replace(/\.json$/, '');
-        });
+        dirname: path.join(__dirname, '/../../lib/collection'),
+        excludeDirs: /^\.(git|svn)$/,
+        recursive: true
+    });
 
     modules = _.mapValues(modules, function (value, key) {
         var name = (key.substr(0, 1).toUpperCase() + key.substr(1)).replace(/-([a-z])/g, function (g) {
@@ -47,10 +39,6 @@ describe('collection module', function () {
 
             it('must have its name defined in the constructor', function () {
                 expect(Module._postman_propertyName).to.be(meta.name);
-            });
-
-            !_.includes(SCHEMALESS_MODULES, meta.name) && it('must have an associated schema file', function () {
-                expect(_.includes(schemas, meta.file)).to.be.ok();
             });
 
             it('must be constructed with no parameter', function () {
