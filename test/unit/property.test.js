b00e58450e9a5128a402f951f1ac7bba5125bb5e
Shamasis Bhattacharya <mail@shamasis.net>
2017-09-04 21:27:35 +0530

    Updated .toObjectResolved to be able to resolve a tree (and a reference tree) of variables. + Updated substitution function to walk up the tree to do partial resolution

--- test/unit/property.test.js
+++ test/unit/property.test.js
@@ -35,19 +35,138 @@ describe('Property', function () {
             expect(property.toObjectResolved.bind(property)).to.throwError();
         });
 
-        it('should resolve properties correctly', function () {
+        it('should resolve and exclude its own .variables', function () {
             var property = new Property(),
                 parent = new Property();
 
-            parent.variables = new VariableList();
-            property.variables = new VariableList({}, [{ key: 'alpha', value: 'foo' }, { key: 'beta', value: 'bar' }]);
+            parent.variables = new VariableList(parent);
+            property.variables = new VariableList(property, [
+                { key: 'alpha', value: 'foo' },
+                { key: 'beta', value: 'bar' }
+            ]);
 
             property.setParent(parent);
+            expect(property.toObjectResolved()).to.eql({});
+        });
+
+        it('should resolve variables defined in its own tree, up one parent', function () {
+            var property = new Property(),
+                parent = new Property();
+
+            parent.variables = new VariableList(parent, [{
+                key: 'var1',
+                value: 'parent-value-1'
+            }, {
+                key: 'var2',
+                value: 'parent-value-2'
+            }]);
+
+            property.setParent(parent);
+            property.testProp1 = 'substituted-{{var1}}';
+            property.testProp2 = 'substituted-{{var2}}';
+
             expect(property.toObjectResolved()).to.eql({
-                variable: [
-                    { type: 'any', value: 'foo', key: 'alpha' },
-                    { type: 'any', value: 'bar', key: 'beta' }
-                ]
+                testProp1: 'substituted-parent-value-1',
+                testProp2: 'substituted-parent-value-2'
+            });
+        });
+
+        it('should resolve variables defined in its own tree, stored within itself', function () {
+            var property = new Property(),
+                parent = new Property();
+
+            property.variables = new VariableList(property, [{
+                key: 'var1',
+                value: 'child-value-1'
+            }]);
+
+            parent.variables = new VariableList(parent, [{
+                key: 'var1',
+                value: 'parent-value-1'
+            }, {
+                key: 'var2',
+                value: 'parent-value-2'
+            }]);
+
+            property.setParent(parent);
+            property.testProp1 = 'substituted-{{var1}}';
+            property.testProp2 = 'substituted-{{var2}}';
+
+            expect(property.toObjectResolved()).to.eql({
+                testProp1: 'substituted-child-value-1',
+                testProp2: 'substituted-parent-value-2'
+            });
+        });
+
+        it('should resolve variables defined in a reference property', function () {
+            var property = new Property(),
+                parent = new Property(),
+                childServices = new Property();
+
+            property.variables = new VariableList(property, [{
+                key: 'var1',
+                value: 'child-value-1'
+            }]);
+
+            parent.variables = new VariableList(parent, [{
+                key: 'var1',
+                value: 'parent-value-1'
+            }, {
+                key: 'var2',
+                value: 'parent-value-2'
+            }]);
+
+            property.setParent(parent);
+            property.testProp1 = 'substituted-{{var1}}';
+            property.testProp2 = 'substituted-{{var2}}';
+
+            childServices.variables = new VariableList(childServices, [{
+                key: 'var1',
+                value: 'external-value-1'
+            }, {
+                key: 'var2',
+                value: 'external-value-2'
+            }]);
+
+            expect(property.toObjectResolved(childServices)).to.eql({
+                testProp1: 'substituted-external-value-1',
+                testProp2: 'substituted-external-value-2'
+            });
+        });
+
+        it('should resolve variables defined in a reference property and ride up its tree', function () {
+            var property = new Property(),
+                parent = new Property(),
+                orphan = new Property();
+
+            property.variables = new VariableList(property, [{
+                key: 'var1',
+                value: 'child-value-1'
+            }]);
+
+            parent.variables = new VariableList(parent, [{
+                key: 'var1',
+                value: 'parent-value-1'
+            }, {
+                key: 'var2',
+                value: 'parent-value-2'
+            }]);
+
+            property.setParent(parent);
+
+            orphan.testProp1 = 'substituted-{{var1}}';
+            orphan.testProp2 = 'substituted-{{var2}}';
+            orphan.variables = new VariableList(orphan, [{
+                key: 'var1',
+                value: 'orphan-value-1'
+            }, {
+                key: 'var2',
+                value: 'orphan-value-2'
+            }]);
+
+            expect(orphan.toObjectResolved(property)).to.eql({
+                testProp1: 'substituted-child-value-1',
+                testProp2: 'substituted-parent-value-2'
             });
         });
     });
