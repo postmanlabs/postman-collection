47fc6d263bc43fccec320ac6fca3495689dddc1e
Udit Vasu <udit.vasu@getpostman.com>
2018-08-08 16:24:55 +0530

    Update unit tests for form-param

--- test/unit/form-param.test.js
+++ test/unit/form-param.test.js
@@ -24,25 +24,29 @@ describe('FormParam', function () {
     });
 
     describe('contentType', function () {
-        it('should parse contentType properly', function () {
+        it('should accept contentType for type `file`', function () {
             var fp = new FormParam({
                 key: 'fileName',
                 src: 'fileSrc',
                 contentType: 'text/csv',
                 type: 'file'
             });
-            expect(fp.toJSON()).to.eql({
-                key: 'fileName',
-                value: '',
-                src: 'fileSrc',
-                contentType: 'text/csv',
-                type: 'file'
+            expect(fp.toJSON()).to.have.property('contentType', 'text/csv');
+        });
+
+        it('should accept contentType for type `text`', function () {
+            var fp = new FormParam({
+                key: 'data',
+                value: '{"foo": "bar"}',
+                contentType: 'application/json',
+                type: 'text'
             });
+            expect(fp.toJSON()).to.have.property('contentType', 'application/json');
         });
 
         it('should default to undefined if contentType is not provided', function () {
             var fp = new FormParam({ key: 'foo', value: 'bar' });
-            expect(fp.toJSON()).to.eql({ key: 'foo', value: 'bar' });
+            expect(fp).to.have.property('contentType', undefined);
         });
     });
 });
