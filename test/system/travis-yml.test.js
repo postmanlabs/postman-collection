ce19371cdb44a128dbe4b733ec284b5a2c1b17ca
Postman <postman@Postmans-MacBook-Pro-2.local>
2018-10-10 11:55:34 +0530

    Updated changes in Travis yml test

--- test/system/travis-yml.test.js
+++ test/system/travis-yml.test.js
@@ -22,25 +22,22 @@ describe('travis.yml', function () {
 
     describe('strucure', function () {
         it('should use the trusty Ubuntu distribution', function () {
-            expect(travisYAML.dist).to.equal('trusty');
+            expect(travisYAML).to.have.property('dist').that.equal('trusty');
         });
 
         it('should have the language set to node', function () {
-            expect(travisYAML.language).to.equal('node_js');
-            expect(travisYAML.node_js).to.eql(['4', '6', '8']);
+            expect(travisYAML).to.have.property('language').that.equal('node_js');
+            expect(travisYAML).to.have.property('node_js').that.eql(['4', '6', '8']);
         });
 
         it('should have a valid before_install sequence', function () {
-            expect(travisYAML.before_install).to.include.keys({
-                0: 'export CHROME_BIN=google-chrome',
-                1: 'export DISPLAY=:99.0',
-                2: 'sh -e /etc/init.d/xvfb start',
-                3: 'sleep 3'
-            });
+            expect(travisYAML).to.have.property('before_install').that.include.ordered.members([
+                'export CHROME_BIN=google-chrome', 'export DISPLAY=:99.0', 'sh -e /etc/init.d/xvfb start', 'sleep 3'
+            ]);
         });
 
         it('should use the stable google chrome package', function () {
-            expect(travisYAML.addons).to.eql({ apt: { packages: ['google-chrome-stable'] } });
+            expect(travisYAML).to.have.property('addons').that.eql({ apt: { packages: ['google-chrome-stable'] } });
         });
     });
 });
