76db6b581c9ddd36b217b38fa886d735986263a0
Postman <postman@Postmans-MacBook-Pro-2.local>
2018-10-11 19:39:27 +0530

    Update header-list tests to chai

--- test/unit/header-list.test.js
+++ test/unit/header-list.test.js
@@ -1,32 +1,32 @@
-var expect = require('expect.js'),
+var expect = require('chai').expect,
     sdk = require('../../lib/index.js'),
 
     HeaderList = sdk.HeaderList;
 
 describe('HeaderList', function () {
     it('should be a working constructor', function () {
-        expect(new HeaderList()).be.a(HeaderList);
+        expect(new HeaderList()).to.be.an.instanceOf(HeaderList);
     });
 
     it('should be able to initialise with header as string', function () {
         var hl = new HeaderList(null, 'Accept: *\nContent-Type: text/html');
-        expect(hl.count()).eql(2);
+        expect(hl.count()).to.equal(2);
     });
 
     it('should be able to export headers to string', function () {
         var hl = new HeaderList(null, 'Accept: *\nContent-Type: text/html');
-        expect(hl.toString()).eql('Accept: *\nContent-Type: text/html');
+        expect(hl.toString()).to.eql('Accept: *\nContent-Type: text/html');
     });
 
     describe('.contentSize', function () {
         it('should be able to return header size', function () {
             var hl = new HeaderList(null, 'Accept: *\nContent-Type: text/html');
-            expect(hl.contentSize(200, 'OK')).eql(38);
+            expect(hl.contentSize(200, 'OK')).to.eql(38);
         });
 
         it('should return 0 for an empty header set', function () {
             var hl = new HeaderList();
-            expect(hl.contentSize()).to.be(0);
+            expect(hl.contentSize()).to.equal(0);
         });
     });
 
@@ -34,10 +34,10 @@ describe('HeaderList', function () {
         it('should correctly identify HeaderList instances', function () {
             var headerList = new HeaderList(null, 'Accept: *\nContent-Type: text/html');
 
-            expect(HeaderList.isHeaderList()).to.eql(false);
-            expect(HeaderList.isHeaderList(headerList)).to.eql(true);
-            expect(HeaderList.isHeaderList({})).to.eql(false);
-            expect(HeaderList.isHeaderList({ _postman_propertyName: 'headerList' })).to.eql(false);
+            expect(HeaderList.isHeaderList()).to.be.false;
+            expect(HeaderList.isHeaderList(headerList)).to.be.true;
+            expect(HeaderList.isHeaderList({})).to.be.false;
+            expect(HeaderList.isHeaderList({ _postman_propertyName: 'headerList' })).to.be.false;
         });
     });
 });
