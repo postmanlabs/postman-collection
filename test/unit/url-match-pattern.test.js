1d8838a990670a88a150f1a71e385010dc0b3760
Postman <postman@Postmans-MacBook-Pro-2.local>
2018-10-11 19:47:14 +0530

    Update url-match-pattern tests to chai

--- test/unit/url-match-pattern.test.js
+++ test/unit/url-match-pattern.test.js
@@ -1,4 +1,4 @@
-var expect = require('expect.js'),
+var expect = require('chai').expect,
     UrlMatchPattern = require('../../lib/url-pattern/url-match-pattern').UrlMatchPattern,
     URL = require('../../lib/collection/url').Url,
 
@@ -13,80 +13,80 @@ var expect = require('expect.js'),
     specForTestMethodOnTarget = function (getTargetForSpec) {
         it('should exit safely with false, when compared against invalid Url', function () {
             var matchPattern = getTargetForSpec('http://*/*');
-            expect(matchPattern.test('google')).to.eql(false);
+            expect(matchPattern.test('google')).to.be.false;
         });
 
         it('should match all urls for <all_urls>', function () {
             var matchPattern = getTargetForSpec('<all_urls>');
-            expect(matchPattern.test('http://www.google.com/')).to.eql(true);
-            expect(matchPattern.test('http://foo.bar.com/')).to.eql(true);
+            expect(matchPattern.test('http://www.google.com/')).to.be.true;
+            expect(matchPattern.test('http://foo.bar.com/')).to.be.true;
         });
 
         it('should match all sdk Url provided', function () {
             var matchPattern = getTargetForSpec('<all_urls>');
-            expect(matchPattern.test(new URL('http://www.google.com/'))).to.eql(true);
-            expect(matchPattern.test(new URL('http://foo.bar.com/'))).to.eql(true);
+            expect(matchPattern.test(new URL('http://www.google.com/'))).to.be.true;
+            expect(matchPattern.test(new URL('http://foo.bar.com/'))).to.be.true;
         });
 
         it('should parse any URL that uses the http protocol', function () {
             var matchPattern = getTargetForSpec('http://*/*');
-            expect(matchPattern.test('http://www.google.com/')).to.eql(true);
-            expect(matchPattern.test('http://foo.bar.com/')).to.eql(true);
+            expect(matchPattern.test('http://www.google.com/')).to.be.true;
+            expect(matchPattern.test('http://foo.bar.com/')).to.be.true;
         });
 
         it('should parse any URL that uses the http protocol, on any host, with path starts with /foo', function () {
             var matchPattern = getTargetForSpec('http://*/foo*');
-            expect(matchPattern.test('http://example.com/foo/bar.html')).to.eql(true);
-            expect(matchPattern.test('http://www.google.com/foo')).to.eql(true);
+            expect(matchPattern.test('http://example.com/foo/bar.html')).to.be.true;
+            expect(matchPattern.test('http://www.google.com/foo')).to.be.true;
         });
 
         it('should parse any URL that uses the https protocol, is on a google.com host', function () {
             var matchPattern = getTargetForSpec('http://*/foo*');
-            expect(matchPattern.test('http://www.google.com/foo/baz/bar')).to.eql(true);
-            expect(matchPattern.test('http://docs.google.com/foobar')).to.eql(true);
+            expect(matchPattern.test('http://www.google.com/foo/baz/bar')).to.be.true;
+            expect(matchPattern.test('http://docs.google.com/foobar')).to.be.true;
         });
 
         it('should parse any URL that uses the http protocol and is on the host 127.0.0.1', function () {
             var matchPattern = getTargetForSpec('http://127.0.0.1/*');
-            expect(matchPattern.test('http://127.0.0.1/')).to.eql(true);
-            expect(matchPattern.test('http://127.0.0.1/foo/bar.html')).to.eql(true);
+            expect(matchPattern.test('http://127.0.0.1/')).to.be.true;
+            expect(matchPattern.test('http://127.0.0.1/foo/bar.html')).to.be.true;
         });
 
         it('should parse any URL that uses the http protocol and is on the host ends with 0.0.1', function () {
             var matchPattern = getTargetForSpec('http://*.0.0.1/');
-            expect(matchPattern.test('http://127.0.0.1/')).to.eql(true);
-            expect(matchPattern.test('http://125.0.0.1/')).to.eql(true);
+            expect(matchPattern.test('http://127.0.0.1/')).to.be.true;
+            expect(matchPattern.test('http://125.0.0.1/')).to.be.true;
         });
 
         it('should parse any URL which has host mail.google.com', function () {
             var matchPattern = getTargetForSpec('*://mail.google.com/*');
-            expect(matchPattern.test('http://mail.google.com/foo/baz/bar')).to.eql(true);
-            expect(matchPattern.test('https://mail.google.com/foobar')).to.eql(true);
+            expect(matchPattern.test('http://mail.google.com/foo/baz/bar')).to.be.true;
+            expect(matchPattern.test('https://mail.google.com/foobar')).to.be.true;
         });
 
         it('Bad Match pattern [No Path]', function () {
             var matchPattern = getTargetForSpec('http://www.google.com');
-            expect(matchPattern.test('http://www.google.com')).to.eql(false);
+            expect(matchPattern.test('http://www.google.com')).to.be.false;
         });
 
         it('Bad Match pattern ["*" in the host can be followed only by a "." or "/"]', function () {
             var matchPattern = getTargetForSpec('http://*foo/bar');
-            expect(matchPattern.test('http://*foo.com')).to.eql(false);
+            expect(matchPattern.test('http://*foo.com')).to.be.false;
         });
 
         it('Bad Match pattern [If "*" is in the host, it must be the first character]', function () {
             var matchPattern = getTargetForSpec('http://foo.*.bar/baz');
-            expect(matchPattern.test('http://foo.z.bar/baz')).to.eql(false);
+            expect(matchPattern.test('http://foo.z.bar/baz')).to.be.false;
         });
 
         it('Bad Match pattern [Missing protocol separator ("/" should be "//")]', function () {
             var matchPattern = getTargetForSpec('http:/bar');
-            expect(matchPattern.test('http:/bar.com')).to.eql(false);
+            expect(matchPattern.test('http:/bar.com')).to.be.false;
         });
 
         it('Bad Match pattern [Invalid protocol', function () {
             var matchPattern = getTargetForSpec('foo://*');
-            expect(matchPattern.test('foo://www.google.com')).to.eql(false);
+            expect(matchPattern.test('foo://www.google.com')).to.be.false;
         });
 
         it('should extract multiple protocols', function () {
@@ -96,7 +96,7 @@ var expect = require('expect.js'),
 
         it('should not match if any one of multiple protocols are invalid', function () {
             var matchPatternObject = new UrlMatchPattern('http+foo+ftp://*/*').createMatchPattern();
-            expect(matchPatternObject).to.eql(undefined);
+            expect(matchPatternObject).to.be.undefined;
         });
     };
 
@@ -147,7 +147,7 @@ describe('UrlMatchPattern', function () {
                     var pattern = specials.join('a'.repeat(3e6)),
                         convertedPattern = new UrlMatchPattern().globPatternToRegexp(pattern);
 
-                    expect(convertedPattern.toString()).to.have.length(33000028);
+                    expect(convertedPattern.toString()).to.have.lengthOf(33000028);
                 });
             });
         });
@@ -156,10 +156,10 @@ describe('UrlMatchPattern', function () {
     describe('createMatchPattern', function () {
         it('should exit with undefined when invoked with an invalid matchPattern', function () {
             var matchPatternObject = new UrlMatchPattern().createMatchPattern();
-            expect(matchPatternObject).to.eql(undefined);
+            expect(matchPatternObject).to.be.undefined;
 
             matchPatternObject = new UrlMatchPattern({}).createMatchPattern();
-            expect(matchPatternObject).to.eql(undefined);
+            expect(matchPatternObject).to.be.undefined;
         });
 
         it('should extract single protocol', function () {
@@ -177,69 +177,81 @@ describe('UrlMatchPattern', function () {
 
         it('should parse any URL that uses the http protocol', function () {
             var matchPatternObject = new UrlMatchPattern('http://*/*').createMatchPattern();
-            expect(matchPatternObject.protocols).to.eql(['http']);
-            expect(matchPatternObject.host).to.eql('*');
-            expect(matchPatternObject.path).to.eql(/^\/.*$/);
+            expect(matchPatternObject).to.deep.include({
+                protocols: ['http'],
+                host: '*',
+                path: /^\/.*$/
+            });
         });
 
         it('should parse any URL that uses the http protocol, on any host, with path starts with /foo', function () {
             var matchPatternObject = new UrlMatchPattern('http://*/foo*').createMatchPattern();
-            expect(matchPatternObject.protocols).to.eql(['http']);
-            expect(matchPatternObject.host).to.eql('*');
-            expect(matchPatternObject.path).to.eql(/^\/foo.*$/);
+            expect(matchPatternObject).to.deep.include({
+                protocols: ['http'],
+                host: '*',
+                path: /^\/foo.*$/
+            });
         });
 
         it('should parse any URL that uses the https protocol, is on a google.com host', function () {
             var matchPatternObject = new UrlMatchPattern('http://*.google.com/foo*bar').createMatchPattern();
-            expect(matchPatternObject.protocols).to.eql(['http']);
-            expect(matchPatternObject.host).to.eql('*.google.com');
-            expect(matchPatternObject.path).to.eql(/^\/foo.*bar$/);
+            expect(matchPatternObject).to.deep.include({
+                protocols: ['http'],
+                host: '*.google.com',
+                path: /^\/foo.*bar$/
+            });
         });
 
         it('should parse any URL that uses the http protocol and is on the host 127.0.0.1', function () {
             var matchPatternObject = new UrlMatchPattern('http://127.0.0.1/*').createMatchPattern();
-            expect(matchPatternObject.protocols).to.eql(['http']);
-            expect(matchPatternObject.host).to.eql('127.0.0.1');
-            expect(matchPatternObject.path).to.eql(/^\/.*$/);
+            expect(matchPatternObject).to.deep.include({
+                protocols: ['http'],
+                host: '127.0.0.1',
+                path: /^\/.*$/
+            });
         });
 
         it('should parse any URL that uses the http protocol and is on the host ends with 0.0.1', function () {
             var matchPatternObject = new UrlMatchPattern('http://*.0.0.1/').createMatchPattern();
-            expect(matchPatternObject.protocols).to.eql(['http']);
-            expect(matchPatternObject.host).to.eql('*.0.0.1');
-            expect(matchPatternObject.path).to.eql(/^\/$/);
+            expect(matchPatternObject).to.deep.include({
+                protocols: ['http'],
+                host: '*.0.0.1',
+                path: /^\/$/
+            });
         });
 
         it('should parse any URL which has host mail.google.com', function () {
             var matchPatternObject = new UrlMatchPattern('*://mail.google.com/*').createMatchPattern();
-            expect(matchPatternObject.protocols).to.eql(['*']);
-            expect(matchPatternObject.host).to.eql('mail.google.com');
-            expect(matchPatternObject.path).to.eql(/^\/.*$/);
+            expect(matchPatternObject).to.deep.include({
+                protocols: ['*'],
+                host: 'mail.google.com',
+                path: /^\/.*$/
+            });
         });
 
         it('Bad Match pattern [No Path]', function () {
             var matchPatternObject = new UrlMatchPattern('http://www.google.com').createMatchPattern();
-            expect(matchPatternObject).to.eql(undefined);
+            expect(matchPatternObject).to.be.undefined;
         });
 
         it('Bad Match pattern ["*" in the host can be followed only by a "." or "/"]', function () {
             var matchPatternObject = new UrlMatchPattern('http://*foo/bar').createMatchPattern();
-            expect(matchPatternObject).to.eql(undefined);
+            expect(matchPatternObject).to.be.undefined;
         });
 
         it('Bad Match pattern [If "*" is in the host, it must be the first character]', function () {
             var matchPatternObject = new UrlMatchPattern('http://foo.*.bar/baz').createMatchPattern();
-            expect(matchPatternObject).to.eql(undefined);
+            expect(matchPatternObject).to.be.undefined;
         });
 
         it('Bad Match pattern [Missing protocol separator ("/" should be "//")]', function () {
             var matchPatternObject = new UrlMatchPattern('http:/bar').createMatchPattern();
-            expect(matchPatternObject).to.eql(undefined);
+            expect(matchPatternObject).to.be.undefined;
         });
 
         it('Bad Match pattern [Invalid protocol', function () {
             var matchPatternObject = new UrlMatchPattern('foo://*').createMatchPattern();
-            expect(matchPatternObject).to.eql(undefined);
+            expect(matchPatternObject).to.be.undefined;
         });
     });
 
@@ -247,8 +259,8 @@ describe('UrlMatchPattern', function () {
         it('should check for protocol in matchPatternObject and returns false for http', function () {
             var matchPattern = new UrlMatchPattern('https://*/*');
 
-            expect(matchPattern.testProtocol('http')).to.eql(false);
-            expect(matchPattern.testProtocol('https')).to.eql(true);
+            expect(matchPattern.testProtocol('http')).to.be.false;
+            expect(matchPattern.testProtocol('https')).to.be.true;
         });
     });
 
@@ -256,18 +268,18 @@ describe('UrlMatchPattern', function () {
         it('should allow /foo and /bar for /*', function () {
             var matchPattern = new UrlMatchPattern('*://*/*');
 
-            expect(matchPattern.testPath('/foo')).to.eql(true);
-            expect(matchPattern.testPath('/bar')).to.eql(true);
-            expect(matchPattern.testPath('/')).to.eql(true);
+            expect(matchPattern.testPath('/foo')).to.be.true;
+            expect(matchPattern.testPath('/bar')).to.be.true;
+            expect(matchPattern.testPath('/')).to.be.true;
         });
 
         it('should allow only /foo and similar paths for /foo*', function () {
             var matchPattern = new UrlMatchPattern('*://*/foo*');
 
-            expect(matchPattern.testPath('/foo')).to.eql(true);
-            expect(matchPattern.testPath('/foo1')).to.eql(true);
-            expect(matchPattern.testPath('/foo/bar')).to.eql(true);
-            expect(matchPattern.testPath('/bar')).to.eql(false);
+            expect(matchPattern.testPath('/foo')).to.be.true;
+            expect(matchPattern.testPath('/foo1')).to.be.true;
+            expect(matchPattern.testPath('/foo/bar')).to.be.true;
+            expect(matchPattern.testPath('/bar')).to.be.false;
         });
     });
 
@@ -275,25 +287,25 @@ describe('UrlMatchPattern', function () {
         it('should match any host', function () {
             var matchPattern = new UrlMatchPattern('*://*/*'),
                 anyhost = matchPattern.testHost(new URL('randomhost.com').getRemote());
-            expect(anyhost).to.eql(true);
+            expect(anyhost).to.be.true;
         });
 
         it('should match host starts with any and have suffix as if in the pattern', function () {
             var matchPattern = new UrlMatchPattern('https://*.foo.com/*'),
                 hostMatched = matchPattern.testHost(new URL('bar.foo.com').getRemote());
-            expect(hostMatched).to.eql(true);
+            expect(hostMatched).to.be.true;
         });
 
         it('should match host ends with a suffix as if in the pattern', function () {
             var matchPattern = new UrlMatchPattern('https://*.foo.com/*'),
                 hostMatched = matchPattern.testHost(new URL('foo.com').getRemote());
-            expect(hostMatched).to.eql(true);
+            expect(hostMatched).to.be.true;
         });
 
         it('should match host with exact as like in match', function () {
             var matchPattern = new UrlMatchPattern('*://foo.com/*'),
                 hostMatched = matchPattern.testHost(new URL('foo.com').getRemote());
-            expect(hostMatched).to.eql(true);
+            expect(hostMatched).to.be.true;
         });
     });
 
@@ -302,13 +314,13 @@ describe('UrlMatchPattern', function () {
         it('should update internal caches between pattern switches', function () {
             var matchPattern = new UrlMatchPattern('http://foo.com/*'),
                 urlMatched = matchPattern.test('http://foo.com');
-            expect(urlMatched).to.eql(true);
+            expect(urlMatched).to.be.true;
 
             matchPattern.update({ pattern: 'http://foo.com/bar*' });
             urlMatched = matchPattern.test('http://foo.com');
-            expect(urlMatched).to.eql(false);
+            expect(urlMatched).to.be.false;
             urlMatched = matchPattern.test('http://foo.com/bar');
-            expect(urlMatched).to.eql(true);
+            expect(urlMatched).to.be.true;
 
         });
     });
