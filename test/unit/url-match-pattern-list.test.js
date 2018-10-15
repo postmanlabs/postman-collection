f1ab18c6fcf835308657b6ebf42bcbd789b3845d
Postman <postman@Postmans-MacBook-Pro-2.local>
2018-10-15 12:17:08 +0530

    Remove done callback in url-match-pattern-list test

--- test/unit/url-match-pattern-list.test.js
+++ test/unit/url-match-pattern-list.test.js
@@ -17,7 +17,7 @@ describe('UrlMatchPatternList', function () {
     describe('test', function () {
         specForTestMethodOnTarget(getTargetForSpec);
 
-        it('should match for any', function (done) {
+        it('should match for any', function () {
             var matchPatternList = new UrlMatchPatternList({}, ['https://example.com/*']);
             expect(matchPatternList.test('https://example.com')).to.be.true;
             expect(matchPatternList.test('https://example.com/')).to.be.true;
@@ -26,30 +26,26 @@ describe('UrlMatchPatternList', function () {
             expect(matchPatternList.test('https://example.com/foo/bar')).to.be.true;
             expect(matchPatternList.test('https://foo.example.com')).to.be.false;
             expect(matchPatternList.test('https://foo.com')).to.be.false;
-            done();
         });
-        it('should match any url for <all_urls>', function (done) {
+        it('should match any url for <all_urls>', function () {
             var matchPatternList = new UrlMatchPatternList({}, ['<all_urls>']);
             expect(matchPatternList.test('https://google.com')).to.be.true;
             expect(matchPatternList.test('https://www.google.com')).to.be.true;
             expect(matchPatternList.test('https://example.com')).to.be.true;
             expect(matchPatternList.test('https://foo.com')).to.be.true;
-            done();
         });
-        it('should match url is pattern list', function (done) {
+        it('should match url is pattern list', function () {
             var matchPatternList = new UrlMatchPatternList({}, ['https://google.com/*', 'https://example.com/*']);
             expect(matchPatternList.test('https://google.com')).to.be.true;
             expect(matchPatternList.test('https://example.com')).to.be.true;
             expect(matchPatternList.test('https://foo.com')).to.be.false;
-            done();
         });
     });
 
     describe('toJSON', function () {
-        it('should return [] when called on an empty UrlMatchPatternList', function (done) {
+        it('should return [] when called on an empty UrlMatchPatternList', function () {
             var patternList = new UrlMatchPatternList();
             expect(patternList.toJSON()).to.eql([]);
-            done();
         });
     });
 });
