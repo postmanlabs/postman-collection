ff006dab7522bd2e379a206527df388c22160e21
Ankit Singh <ankitsiam@gmail.com>
2019-01-11 18:39:40 +0530

    Test : SshAuth for sanity

---
+++ test/unit/ssh-auth.test.js
@@ -0,0 +1,60 @@
+var expect = require('chai').expect,
+    SshAuth = require('./../../lib/collection/ssh-auth').SshAuth,
+    E = '',
+    DEFAULT_SSH_PORT = 22,
+
+    authConfig = {
+        host: '127.0.0.1',
+        port: 23,
+        username: 'ubuntu',
+        userPassword: 'someSecurepass',
+        privateKeyPath: './path/to/keyFile'
+    };
+
+describe('SSH Auth', function () {
+    describe('sanity', function () {
+        it('should initialize the values to their defaults', function () {
+            var ssha = new SshAuth();
+
+            expect(ssha).to.deep.include({
+                host: undefined,
+                port: DEFAULT_SSH_PORT,
+                username: E,
+                userPassword: E,
+                privateKeyPath: E,
+                passPhrase: E
+            });
+        });
+
+        it('should prepopulate the values when pass through the constructor', function () {
+            var ssha = new SshAuth(authConfig);
+
+            expect(ssha).to.deep.include(authConfig);
+        });
+    });
+
+    describe('toJSON', function () {
+        it('should retain properties from original json', function () {
+            var ssha = new SshAuth(authConfig),
+                serialisedConfig = ssha.toJSON();
+
+            expect(serialisedConfig).to.deep.include(authConfig);
+        });
+    });
+
+    describe('isSshAuth', function () {
+        it('should correctly identify SshAuth objects', function () {
+            var ssha = new SshAuth(authConfig);
+
+            expect(SshAuth.isSshAuth(ssha)).to.be.true;
+        });
+
+        it('correctly identify non SshAuth objects', function () {
+            expect(SshAuth.isSshAuth({})).to.be.false;
+        });
+
+        it('should return false when called without arguments', function () {
+            expect(SshAuth.isSshAuth()).to.be.false;
+        });
+    });
+});
