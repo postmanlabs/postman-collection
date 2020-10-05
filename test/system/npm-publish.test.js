76609296af781fa8a50acb195e2569628f3f4b5f
Udit Vasu <udit@pm.me>
2020-10-06 01:05:43 +0530

    Update system test

--- test/system/npm-publish.test.js
+++ test/system/npm-publish.test.js
@@ -1,8 +1,13 @@
 const expect = require('chai').expect,
     // eslint-disable-next-line security/detect-child-process
-    exec = require('child_process').execSync;
+    exec = require('child_process').execSync,
+
+    nodeVersion = parseInt(process.versions.node);
 
 describe('npm publish', function () {
+    // @todo remove when drop support for Node < 10
+    if (nodeVersion < 10) { return; }
+
     const packageInfo = JSON.parse(exec('npm pack --dry-run --json'))[0],
         packagedFiles = packageInfo.files.map(({ path }) => { return path; });
 
