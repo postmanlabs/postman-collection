eb0a62fd22ec847f272b0bdd8223cac3fc5f2e55
Udit Vasu <udit@pm.me>
2021-06-21 19:07:08 +0530

    Update build-types script

--- npm/build-types.js
+++ npm/build-types.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
 // ---------------------------------------------------------------------------------------------------------------------
-// This script is intended to generate type-definition for this module
+// This script is intended to generate type-definition for this module.
 // ---------------------------------------------------------------------------------------------------------------------
 
-/* eslint-env node, es6 */
-require('shelljs/global');
-
-var path = require('path'),
+const path = require('path'),
     fs = require('fs'),
     pkg = require('../package.json'),
     chalk = require('chalk'),
-
+    { exec, rm, test } = require('shelljs'),
 
     IS_WINDOWS = (/^win/).test(process.platform),
     TARGET_DIR = path.join('types'),
@@ -25,7 +22,7 @@ var path = require('path'),
 `;
 
 module.exports = function (exit) {
-    console.log(chalk.yellow.bold('Generating type-definitions...'));
+    console.info(chalk.yellow.bold('Generating type-definitions...'));
 
     try {
         // clean directory
@@ -33,24 +30,23 @@ module.exports = function (exit) {
     }
     catch (e) {
         console.error(e.stack || e);
+
         return exit(e ? 1 : 0);
     }
 
     exec(`${IS_WINDOWS ? '' : 'node'} ${path.join('node_modules', '.bin', 'jsdoc')}${IS_WINDOWS ? '.cmd' : ''}` +
         ' -c .jsdoc-config-type-def.json -p', function (code) {
-
         if (!code) {
             fs.readFile(`${TARGET_DIR}/index.d.ts`, function (err, contents) {
                 if (err) {
-                    console.log(chalk.red.bold('unable to read the type-definition file'));
+                    console.info(chalk.red.bold('unable to read the type-definition file'));
                     exit(1);
                 }
                 var source = contents.toString();
+
                 source = source
                     // replace all declare keyword with export, as the whole typedef will be wrapped around a module
                     .replace(/^declare /gm, 'export ')
-                    // replacing Integer with number as 'Integer' is not a valid data-type in Typescript
-                    .replace(/Integer/gm, 'number')
                     // replacing String[] with string[] as 'String' is not a valid data-type in Typescript
                     .replace(/String\[]/gm, 'string[]')
                     // replacing Boolean[] with boolean[] as 'Boolean' is not a valid data-type in Typescript
@@ -67,21 +63,21 @@ module.exports = function (exit) {
 
                 fs.writeFile(`${TARGET_DIR}/index.d.ts`, source, function (err) {
                     if (err) {
-                        console.log(chalk.red.bold('unable to write the type-definition file'));
+                        console.info(chalk.red.bold('unable to write the type-definition file'));
                         exit(1);
                     }
-                    console.log(chalk.green.bold(`Type-definition file saved successfully at "${TARGET_DIR}"`));
+                    console.info(chalk.green.bold(`Type-definition file saved successfully at "${TARGET_DIR}"`));
                     exit(0);
                 });
             });
         }
         else {
             // output status
-            console.log(chalk.red.bold('unable to generate type-definition'));
+            console.info(chalk.red.bold('unable to generate type-definition'));
             exit(code);
         }
     });
 };
 
 // ensure we run this script exports if this is a direct stdin.tty run
-!module.parent && module.exports(exit);
+!module.parent && module.exports(process.exit);
